17a35de7b2fdf4df3c4f5e1d742c1cc96c74e944
wvdbrande <wim.vandenbrande@one-agency.be>
2017-04-06 10:59:09 +0200

    First version of restClient.

---
+++ src/main/java/be/ictdynamic/RestClientApplication.java
@@ -0,0 +1,11 @@
+package be.ictdynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientApplication.class, args);
+	}
+}
